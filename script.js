87e7b096100a2427ef65d1fd5667c6ff1b15926a
Isaac Duong <isaacduong157@gmail.com>
2023-08-19 12:00:28 +1000

    Implement addBookToLibrary function

---
+++ script.js
@@ -0,0 +1,16 @@
+const bookDisplay = document.getElementsByClassName('bookDisplay');
+
+const myLibrary = [];
+
+function Book() {
+    // the constructor...
+}
+
+function addBookToLibrary(bookName, bookAuthor, bookPages, haveRead) {
+    // do stuff here
+    myLibrary.push([bookName, bookAuthor, bookPages, haveRead]);
+}
+
+function displayBooks () {
+
+}
